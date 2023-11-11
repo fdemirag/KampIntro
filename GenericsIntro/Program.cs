60217c333dd4d11504f8a80d17410375016d6dff
Fatih Demirağ <fatihdemirag@Fatihs-MacBook-Air.local>
2023-11-12 00:31:50 +0300

    Genericler ile calisildi

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,14 @@
+﻿using GenericsIntro;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        MyList<string> isimler = new MyList<string>();
+        isimler.Add("Engin");
+
+        List<string> liste = new List<string>();
+        Console.WriteLine(liste.Count);
+
+    }
+}
