1637ab375f9648c747ad68059ec8e41ceba59fb2
Fatih Demirağ <fatihdemirag@Fatihs-MacBook-Air.local>
2023-11-11 20:28:02 +0300

    Metodlar ile matematik islemi yapildi

---
+++ Math/Program.cs
@@ -0,0 +1,11 @@
+﻿using Math;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DortIslem dortIslem = new DortIslem();
+        dortIslem.Topla(4, 5);
+        dortIslem.Topla(9, 10);
+    }
+}
